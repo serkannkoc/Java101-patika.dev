be5aeaeb218b60926c13567bc44a02946d441319
Serkan Koç <serkan.koc@marun.edu.tr>
2022-04-22 02:40:44 +0300

    Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.

---
+++ src/Loops/SumOfOddNumbers.java
@@ -0,0 +1,20 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k;
+        int sum = 0;
+        while (true){
+            System.out.print("Enter a number: ");
+            k = input.nextInt();
+            if(k %4 == 0)
+                sum += k;
+            if(k % 2 == 1)
+                break;
+        }
+        System.out.print("Sum: "+sum);
+    }
+}
