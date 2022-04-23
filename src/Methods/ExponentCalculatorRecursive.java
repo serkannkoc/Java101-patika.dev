bb3dbcc41faa20135efd639c1f29a502151073f2
Serkan Koç <serkan.koc@marun.edu.tr>
2022-04-24 01:54:52 +0300

    reformat code

--- src/Methods/ExponentCalculatorRecursive.java
+++ src/Methods/ExponentCalculatorRecursive.java
@@ -1,8 +1,15 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class ExponentCalculatorRecursive {
     public static void main(String[] args) {
-        System.out.println(exponent(3, 4));
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        System.out.print("Enter the power: ");
+        int power = input.nextInt();
+        System.out.println(exponent(number, power));
     }
 
     static int exponent(int number, int exponent) {
