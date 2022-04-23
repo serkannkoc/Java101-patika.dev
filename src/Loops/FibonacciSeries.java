352b4cce93e84a57bec29eca286a6e3a0cc05b38
Serkan Koç <serkan.koc@marun.edu.tr>
2022-04-23 15:45:08 +0300

    fibonacci series is finished.

---
+++ src/Loops/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int count = input.nextInt();
+        int n1 = 0, n2 = 1, n3, i;
+        System.out.print(n1 + " " + n2);
+
+        for (i = 2; i < count; ++i) {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
