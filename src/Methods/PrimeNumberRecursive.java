bf3b9adccf4ae206671287a1fa0c4758d554cc42
Serkan Koç <serkan.koc@marun.edu.tr>
2022-04-24 02:01:46 +0300

    prime number recursive method

---
+++ src/Methods/PrimeNumberRecursive.java
@@ -0,0 +1,22 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class PrimeNumberRecursive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        System.out.println(isPrime(number, 2));
+    }
+
+    static boolean isPrime(int n, int i) {
+        if (n <= 2)
+            return n == 2;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+        return isPrime(n, i + 1);
+    }
+}
