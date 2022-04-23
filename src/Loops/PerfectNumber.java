6a25e68201bfe761f1df97e272215d58c98c1cbe
Serkan Koç <serkan.koc@marun.edu.tr>
2022-04-23 15:20:08 +0300

    Perfect number calculator is finished.

---
+++ src/Loops/PerfectNumber.java
@@ -0,0 +1,20 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int sum=0;
+        for(int i=number-1;i>0;i--){
+            if(number%i == 0)
+                sum += i;
+        }
+        if(sum == number)
+            System.out.println(number+" is a perfect number.");
+        else
+            System.out.println(number+" is not a perfect number.");
+    }
+}
